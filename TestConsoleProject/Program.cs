2c6af4ba957038a7b8efe5667fd2bb77fcaa1aa5
dmytro klymiuk <dimon4egkl55@gmail.com>
2023-10-05 17:21:46 +0300

    add git clone functionality to console app

--- TestConsoleProject/Program.cs
+++ TestConsoleProject/Program.cs
@@ -3,46 +3,81 @@ using System.Globalization;
 using LibGit2Sharp;
 
 //const string pathToRepository = "/home/dmytro/work/poc-git-library/test_git_library";
-const string pathToRepository = "/app/test_git_library";
+//const string pathToRepository = "/home/dmytro/work/poc-git-library/git_clone_automated/libgit2sharpTest";
+const string pathToRepository = "/app/libgit2sharpTest";
+var passwordCredentials = new UsernamePasswordCredentials()
+{
+    Username = "dimon4egkl55@gmail.com",
+    Password = "password"
+};
 
+if (Directory.Exists(pathToRepository))
+{
+    Directory.Delete(pathToRepository, true);
+}
 Stopwatch stopwatch = new();
 stopwatch.Start();
+Clone(passwordCredentials, pathToRepository);
+stopwatch.Stop();
+Console.WriteLine($"Repository cloned in {stopwatch.Elapsed.TotalSeconds} seconds.");
+
+stopwatch.Restart();
 using var repository = new Repository(pathToRepository);
-
-RepositoryStatus repositoryStatus = repository.RetrieveStatus();
-DateTime localDate = DateTimeOffset.Now.LocalDateTime;
-var contentOfFile = $"content with timestamp {localDate}";
-File.WriteAllText($"{pathToRepository}/test.txt", contentOfFile);
-
-repository.Index.Add("test.txt");
-repository.Index.WriteToTree(); // totally need this one
-
-
-Signature author = new Signature("dmytro", "dimon4egkl55@gmail.com", localDate);
-var commitMessage = $"another test commit {localDate}";
-var newCommit = repository.Commit(commitMessage, author, author);
-Console.WriteLine($"Committed with message: {commitMessage} and commitId: {newCommit.Id}");
-var options = new PushOptions
-{
-    CredentialsProvider = (_,_,_) =>
-        new UsernamePasswordCredentials()
-        {
-            Username = "dimon4egkl55@gmail.com",
-            Password = "password"
-        }
-};
-repository.Network.Push(repository.Branches["main"], options);
-Console.WriteLine("Commit was pushed");
+Commit(pathToRepository, repository);
+Push(passwordCredentials, repository);
+DumpHistory(repository);
 stopwatch.Stop();
 
-IEnumerable<Commit> lastCommits = repository.Commits.Take(15);
-Console.WriteLine("Dumping commits history...");
-foreach (var commit in lastCommits)
+Console.WriteLine($"Connecting, commiting, pushing and dumping logs took: {stopwatch.Elapsed.Milliseconds} milliseconds.");
+
+void Commit(string s, Repository repo)
 {
-    Console.WriteLine(string.Format(CultureInfo.InvariantCulture,
-        "{0} {1}",
-        commit.Id.ToString(7),
-        commit.MessageShort));
+    DateTime localDate = DateTimeOffset.Now.LocalDateTime;
+    var contentOfFile = $"content with timestamp {localDate}";
+    File.WriteAllText($"{s}/test.txt", contentOfFile);
+    RepositoryStatus repositoryStatus = repo.RetrieveStatus();
+    repo.Index.Add("test.txt");
+    repo.Index.WriteToTree(); // totally need this one
+
+
+    Signature author = new Signature("dmytro", "dimon4egkl55@gmail.com", localDate);
+    var commitMessage = $"another test commit {localDate}";
+    var newCommit = repo.Commit(commitMessage, author, author);
+    Console.WriteLine($"Committed with message: {commitMessage} and commitId: {newCommit.Id}");
 }
 
-Console.WriteLine($"Execution took: {stopwatch.Elapsed.Milliseconds}");
+void Push(UsernamePasswordCredentials usernamePasswordCredentials, Repository repository2)
+{
+    var options = new PushOptions
+    {
+        CredentialsProvider = (_, _, _) => usernamePasswordCredentials
+    };
+    repository2.Network.Push(repository2.Branches["master"], options);
+    Console.WriteLine("Commit was pushed");
+}
+
+void DumpHistory(Repository repository3)
+{
+    IEnumerable<Commit> lastCommits = repository3.Commits.Take(15);
+    Console.WriteLine("Dumping commits history...");
+    foreach (var commit in lastCommits)
+    {
+        Console.WriteLine(string.Format(CultureInfo.InvariantCulture,
+            "{0} {1}",
+            commit.Id.ToString(7),
+            commit.MessageShort));
+    }
+}
+
+void Clone(UsernamePasswordCredentials passwordCredentials1, string pathToRepository1)
+{
+    CloneOptions cloneOptions = new CloneOptions();
+    cloneOptions.CredentialsProvider =
+        (_, _, _) => passwordCredentials1;
+
+    Console.WriteLine("Cloning the repository...");
+    Repository.Clone(
+        "https://github.com/dimon4egkl/libgit2sharpTest.git",
+        pathToRepository1,
+        cloneOptions);
+}
